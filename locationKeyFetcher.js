66c5fa609acbd70bf1351e5ebadfbfa7904636ad
gorenbsl <gorenbsl22@gmail.com>
2025-04-08 16:59:58 +0530

    Update locationKeyFetcher.js

--- locationKeyFetcher.js
+++ locationKeyFetcher.js
@@ -1,27 +1,17 @@
-
-// Fetch location keys for each taluka using AccuWeather API
-const fetch = require('node-fetch');
-
 const talukas = [
   "Jalgaon", "Bhusawal", "Amalner", "Chopda", "Yaval", "Raver", "Pachora", "Jamner",
   "Bodwad", "Erandol", "Parola", "Dharangaon", "Chalisgaon", "Bhadgaon", "Muktainagar"
 ];
 
 const apiKey = "AmpqFspNOVCRZdhjUXtuAOCWbKwT4Ok9";
-const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
 
-async function getLocationKeys() {
-  let keys = {};
-  for (let i = 0; i < talukas.length; i++) {
-    const name = talukas[i];
-    const res = await fetch(`${baseUrl}?apikey=${apiKey}&q=${name},IN`);
-    const data = await res.json();
-    if (data && data.length > 0) {
-      keys[name] = data[0].Key;
-    }
-    await new Promise(r => setTimeout(r, 500)); // avoid rate limit
+async function fetchLocationKeys() {
+  for (const taluka of talukas) {
+    const response = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${taluka}%20Jalgaon`);
+    const data = await response.json();
+    const key = data[0]?.Key ?? "Not found";
+    console.log(`"${taluka}": "${key}",`);
   }
-  console.log(keys);
 }
 
-getLocationKeys();
+fetchLocationKeys();
