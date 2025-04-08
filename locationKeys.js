91d35b2cca4f9da23b7a5c5bb299e2a70f1f3d11
gorenbsl <gorenbsl22@gmail.com>
2025-04-08 13:07:39 +0530

    Create locationKeys.js

---
+++ locationKeys.js
@@ -0,0 +1,17 @@
+const locationKeys = {
+  "Jalgaon": "202396",
+  "Bhusawal": "202400",
+  "Amalner": "202402",
+  "Chopda": "202405",
+  "Yaval": "202408",
+  "Raver": "202410",
+  "Pachora": "202412",
+  "Jamner": "202415",
+  "Bodwad": "202417",
+  "Erandol": "202420",
+  "Parola": "202422",
+  "Dharangaon": "202425",
+  "Chalisgaon": "202427",
+  "Bhadgaon": "202430",
+  "Muktainagar": "202433"
+};
